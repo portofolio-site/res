1cab29bacb425db4a8aa172e2db9b637275ac6da
portofolio-site <74216770+portofolio-site@users.noreply.github.com>
2020-12-14 10:13:55 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>はじめてのWordPress</title>
+        <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.9.1/build/cssreset/cssreset-min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="header" class="container">
+            <h1><a href="">はじめてのWordPress</a></h1>
+            <ul class="menu">
+                <li><a href="">menu</a></li>
+                <li><a href="">menu</a></li>
+                <li><a href="">menu</a></li>
+            </ul>
+        </div><!-- /header -->
+        <div id="main" class="container">
+            <div id="posts">
+                <div class="post">
+                    <div class="post-header">
+                        <h2>
+                            <a href="">タイトル</a>
+                        </h2>
+                        <div class="post-meta">
+                            2013年4月3日 【カテゴリ】
+                        </div>
+                    </div>
+                    <div class="post-content">
+                        <div class="post-image">
+                            <img src="noimage.png" width="100" height="100">
+                        </div>
+                        <div class="post-body">
+                            <p>本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。<p>
+                        </div>
+                    </div>
+                </div><!-- /post -->
+                <div class="navigation">
+                    <div class="prev">prev</div>
+                    <div class="next">next</div>
+                </div>
+            </div><!-- /posts -->
+            <div id="sidebar">
+                <div class="widget">
+                    <h3>カテゴリー</h3>
+                    <ul>
+                        <li><a href="">item</a></li>
+                        <li><a href="">item</a></li>
+                        <li><a href="">item</a></li>
+                    </ul>
+                </div>
+            </div><!-- /sidebar -->
+        </div><!-- /main -->
+        <div id="footer" class="container">
+            footer
+        </div><!-- /footer -->
+    </body>
+</html>
